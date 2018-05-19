99063b4efe1d475a66cae52baf37bfd047333b82
Scott McCrory <scott@mccrory.us>
2018-05-19 09:58:39 -0400

    Added spring stuff

---
+++ src/main/java/us/mccrory/rpijavacamera/RpiJavaCameraApplication.java
@@ -0,0 +1,12 @@
+package us.mccrory.rpijavacamera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpiJavaCameraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RpiJavaCameraApplication.class, args);
+	}
+}
