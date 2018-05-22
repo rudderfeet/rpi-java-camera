e0a0496c5a98f0c8923fa216b82950c7288a3258
Scott McCrory <scott@mccrory.us>
2018-05-22 16:03:49 -0400

    Hmm, where is the image being stored?

--- src/main/java/us/mccrory/rpijavacamera/RpiJavaCameraApplication.java
+++ src/main/java/us/mccrory/rpijavacamera/RpiJavaCameraApplication.java
@@ -31,13 +31,8 @@ public class RpiJavaCameraApplication {
 			}
 
 			// Create a Camera that saves images to the Pi's Pictures directory.
-			System.out.println("Let's take a picture and store it in /tmp...");
-			RPiCamera piCamera = new RPiCamera("/tmp");
-			piCamera.setWidth(500).setHeight(500) // Set Camera to produce 500x500 images.
-		    .setBrightness(75)                // Adjust Camera's brightness setting.
-		    .setExposure(Exposure.AUTO)       // Set Camera's exposure.
-		    .setTimeout(2)                    // Set Camera's timeout.
-		    .setAddRawBayer(true);            // Add Raw Bayer data to image files created by Camera.
+			System.out.println("Let's take a picture...");
+			RPiCamera piCamera = new RPiCamera();
 			piCamera.takeStill("rpi-java-camera.jpg");
 			
 			System.out.println("ALL DONE!");
